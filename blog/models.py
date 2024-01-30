8af0311c5609d3ca94a3c44550679239b7fe086a
salman <therodfighter@gmail.com>
2024-01-30 05:22:59 +0400

    mb

--- blog/models.py
+++ blog/models.py
@@ -1,30 +1,36 @@
 from django.db import models
-from core.models import Seo
 from ckeditor.fields import RichTextField
 
 # Create your models here.
 
 
-class Category(models.Model):
+class common(models.Model):
+    title = models.CharField(max_length=70, blank=True, null=True)
+    keywordz = models.CharField(max_length=70, blank=True, null=True)
+    desc = models.CharField(max_length=150, blank=True, null=True)
+    struct_data = models.CharField(max_length=400, blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(common):
     name = models.CharField(max_length=30, unique=True)
-    seo = models.OneToOneField(
-        Seo, verbose_name="Seo", on_delete=models.CASCADE, blank=True, null=True
+    slug = models.SlugField(
+        max_length=150,
+        unique=True,
     )
 
 
-class Tag(models.Model):
+class Tag(common):
     name = models.CharField(max_length=30, unique=True)
-    seo = models.OneToOneField(
-        Seo, verbose_name="Seo", on_delete=models.CASCADE, blank=True, null=True
-    )
 
 
-class Blog(models.Model):
+class Blog(common):
     title = models.CharField(max_length=150, unique=True)
     slug = models.SlugField(
         max_length=150,
         unique=True,
-        help_text="This will be auto generated if you dont add it",
     )
     short_desc = models.CharField(max_length=150, blank=True, null=True)
     content = RichTextField()
@@ -49,11 +55,4 @@ class Blog(models.Model):
     tags = models.ManyToManyField(
         Tag,
         verbose_name="tags",
-    )
-    seo = models.OneToOneField(
-        Seo,
-        verbose_name="Seo",
-        on_delete=models.CASCADE,
-        blank=True,
-        null=True,
     )
--- core/models.py
+++ core/models.py
@@ -1,13 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-
-class Seo(models.Model):
-    title = models.CharField(max_length=70, blank=True, null=True)
-    keyword = models.CharField(max_length=70, blank=True, null=True)
-    desc = models.CharField(max_length=150, blank=True, null=True)
-    image = models.ImageField(
-        upload_to="seo/images", max_length=70, blank=True, null=True
-    )
-    struct_data = models.CharField(max_length=400, blank=True, null=True)
